c8f1d679e1f433b18f92eafcb7d02b51599500fa
shakhzod-star <77914936+shakhzod-star@users.noreply.github.com>
2022-11-10 10:28:38 +0500

    last change

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,26 @@
 <script setup lang="ts">
 import { ref } from "vue";
-// import baseInput from "./components/baseInput.vue";
-// import Dropdown from "./components/Dropdown.vue";
-// import Footer from './components/FooterBottom.vue'
-// import {ref ,reactive, onMounted , computed , watch,} from 'vue';
-// import {ref} from 'vue'
-// import HelloWorld from './components/HelloWorld.vue';
-//  <HelloWorld msg="Vite + Vue" HelloNumber='12' @emit-fn="gg"    />
-// function gg(params: number) {
-//   console.log(params);
-// }
-// enum status {
-//   COMPLATE = 'selected video Category',
-// }
-// interface cardWord {
-//   date: string; //September 13,2022
-//   paragraf: string; //Bringing down the Digital Gap in Higher Education
-//   btn: string; //Read full article
-// }
-// interface Card {
-//   name: string;
-//   whoIs: string;
-//   comment: string;
-// }
-// const cards = <Card>{
-//   name: "Ubaydulla Salokhiddinov",
-//   whoIs: "Project manager",
-//   comment:
-//     "Since switching to WiseSpace, we have increased our use of e-learning by 100%, largely because it is so simple for our teachers, students and administrative staff",
-// };
+const colors: string[] =  ['pink','green','yellow','blue']
+let show = ref<boolean>(true)
 
-// defineProps<Card>()
- const name = ref<string>('')
-interface arrays {
-  name: string;
-}
-const Options: arrays[] = [
-  {
-    name: "Film",
-  },
-  {
-    name: "Multfilm",
-  },
-  {
-    name: "comedy",
-  },
-];
-// const active = ref<boolean>(false)
 </script>
 
 <template>
   <div>
-    <div class="flex overflow-hidden max-w-[738px] relative">
-      <img class="default_one" src="../src/assets/img/feature.png" alt="">
-      <img class="default_two" src="../src/assets/img/feature.png" alt="">
+    <!--1 <div v-hello="'Nodir'"></div> -->
+    <!--2 <div v-switch-color="['pink','green','yellow','blue']">Vue school</div> -->
+    <!--3 <div v-switch-color:slow="colors">Vue school</div> -->
+     <!-- <div v-switch-color.textcenter.underline="colors">Vue school</div> -->
+    <div  v-switching-color:fast.textcenter="colors"
+      v-if="show"
+    >
+      Vue School
     </div>
-    <!-- <button @click="active = !active">Click to ACtive</button> -->
+    <button @click="show = !show">toggle</button>
+
   </div>
 </template>
 
 <style>
-.default_one {
-  animation: default_one 4s infinite;
-  animation-delay: 4s;
-}
-.default_two {
-  animation: default_two 4s infinite;
-}
-@keyframes default_one {
-  0% {  transform: translateX(0px);}
-  50% {transform: translateX(738px);}
-  100% { transform: translateX(0px);}
-}
-@keyframes default_two {
-  0% {  transform: translateX(738px);}
-  50% { transform: translateX(0px)}
-  99% { transform: translateX(-738px)}
-  100% { transform: translateX(-738px);}
-}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,56 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.directive('hello', function (params:any,val:any) {
+    params.style.color = 'red'
+    console.log(params.innerHTML =  `${val.value} nma gap ozi`);
+})
+app.directive( 'switching-color', {
+  bind  (val:any, binding:any ){
+    const modifiers = binding.modifiers
+    if (modifiers.underline){
+     val.style.textDecoration = 'underline'
+    }
+    if (modifiers.textcenter) {
+     val.style.textAlign = 'center'
+   }
+     interface spd {
+       [propName: string]:number; 
+     }
+     const speeds:spd = {
+       slow : 10000,
+       normal : 5000,
+       fast : 500 ,
+       crazy : 100
+     }
+     console.log(binding);
+     const speedName = binding.arg || 'normal'
+     const speed   = speeds[speedName]
+           
+           const colors = binding.value
+           let i = 0
+           val.__SwitchingColorInterval__= setInterval(() => {
+            console.log('🎨 coloring')
+              val.style.color = colors[i++]
+              if (i > colors.length - 1) {
+                i = 0
+              }
+            }, speed)
+          },
+   unbind (val : any) {
+    clearInterval(val.__SwitchingColorInterval__)
+  }
+   
+  
+})
+
+
+
+
+
+
+
+
+app.mount('#app')
